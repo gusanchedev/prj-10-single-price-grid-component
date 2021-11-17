8bc0808d91f8474a7ecabff0587e4aab5369961f
Gustavo Sanchez <gustavosanchezgalarza@gmail.com>
2021-11-17 06:56:18 -0500

    💄  Add styling for 375px screen

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ module.exports = {
     extend: {
       fontFamily: {
         "karla": ["karla"],
-      }
+      },
+      colors: {
+        "myCyan": "hsl(179, 62%, 43%)",
+        "myBrightYellow": "hsl(71, 73%, 54%)",
+        "myLightGray": "hsl(204, 43%, 93%)",
+        "myGrayishBlue": "hsl(218, 22%, 67%)",
+        "myGreenLight": "hsla(179,47%, 52%, 1)",
+        "myGreenDark": "hsla(179,61%, 44%, 1)",
+      },
     },
   },
   variants: {
